d8d9c96d4f70015505336c4fb02189afc9bc79cd
Shuai Li <22931431+lishuaiphd@users.noreply.github.com>
2025-03-24 08:51:56 +0100

    Add dropout, weight decay, evaluation strategy (#3)

--- src/model/loaders/causallmloader.py
+++ src/model/loaders/causallmloader.py
@@ -1,9 +1,12 @@
-from transformers import AutoModelForCausalLM
+from transformers import AutoModelForCausalLM, AutoConfig
 
 
 class CausalLmLoader:
     def __init__(self, model_path):
         self.model_path = model_path
 
-    def load_model(self):
+    def load_model(self, hidden_dropout_prob=0, attention_probs_dropout_prob=0):
+        config = AutoConfig.from_pretrained(self.model_path)
+        config.hidden_dropout_prob = hidden_dropout_prob
+        config.attention_probs_dropout_prob = attention_probs_dropout_prob
         return AutoModelForCausalLM.from_pretrained(self.model_path)
